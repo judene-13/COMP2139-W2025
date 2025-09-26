ffade097528cfc60540f97abeb5d804627e03c23
Judene Brown <judenebrown13@gmail.com>
2025-09-26 09:37:41 -0400

    lab 2 & 3part1 (had issues with it sorry for delay)

---
+++ COMP2139-ICE/Controllers/ProjectController.cs
@@ -0,0 +1,60 @@
+using COMP2139_ICE.Data;
+using COMP2139_ICE.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP2139_ICE.Controllers;
+
+public class ProjectController : Controller
+
+{
+    
+    private readonly ApplicationDbContext _context;
+
+    public ProjectController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    
+
+    [HttpGet]
+    public IActionResult Index()
+
+    {
+        /*
+        var projects = new List<Project>()
+        {
+            new Project { ProjectId = 1, Name = "Project 1", Description = "First Project" }
+        };
+        */
+        var projects = _context.Projects.ToList(); //lab 3
+        return View(projects);
+
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+
+    [HttpPost]
+    public IActionResult Create(Project project)
+    {
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet]
+    public IActionResult Details(int id)
+    {/*
+        var project = new Project { ProjectId = id, Name = "Project " + id, Description = "Details of Project" + id };
+        */
+        var project= _context.Projects.FirstOrDefault(p=>p.ProjectId == id);
+        if (project == null)
+        {
+            return NotFound();
+        }
+        return View(project);
+    }
+}
+
--- COMP2139-ICE/Program.cs
+++ COMP2139-ICE/Program.cs
@@ -1,7 +1,13 @@
+using COMP2139_ICE.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options=>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -24,6 +30,6 @@ app.MapControllerRoute(
         name: "default",
         pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
+//
 
 app.Run();
