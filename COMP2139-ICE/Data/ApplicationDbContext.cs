5b8578ad64de7084538826995c852b5dc286821f
Judene Brown <judenebrown13@gmail.com>
2025-09-26 09:36:15 -0400

    lab 2 &3 part 1 (had issues with lab 3 sorry for the delay)

---
+++ COMP2139-ICE/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+using COMP2139_ICE.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace COMP2139_ICE.Data;
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+{
+}
+public DbSet<Project>Projects{ get; set; }
+
+}
